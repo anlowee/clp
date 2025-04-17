b6ea290e7612d49bd6013f0ab18b25155e419564
wraymo <37269683+wraymo@users.noreply.github.com>
2025-04-17 17:23:49 -0400

    refactor(clp-s): Split some of `Output`'s responsibilities into a new `QueryRunner` class. (#817)

--- components/core/src/clp_s/clp-s.cpp
+++ components/core/src/clp_s/clp-s.cpp
@@ -241,8 +241,11 @@ bool search_archive(
     }
 
     // Narrow against schemas
-    SchemaMatch match_pass(archive_reader->get_schema_tree(), archive_reader->get_schema_map());
-    if (expr = match_pass.run(expr); std::dynamic_pointer_cast<ast::EmptyExpr>(expr)) {
+    auto match_pass = std::make_shared<SchemaMatch>(
+            archive_reader->get_schema_tree(),
+            archive_reader->get_schema_map()
+    );
+    if (expr = match_pass->run(expr); std::dynamic_pointer_cast<ast::EmptyExpr>(expr)) {
         SPDLOG_INFO("No matching schemas for query '{}'", query);
         return true;
     }
@@ -328,7 +331,6 @@ bool search_archive(
             match_pass,
             expr,
             archive_reader,
-            timestamp_dict,
             std::move(output_handler),
             command_line_arguments.get_ignore_case()
     );
